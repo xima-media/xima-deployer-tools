cf3143a63bd2580e3858c0baa6d4f130dd0860e9
Tobias Gaertner <tga@xima.de>
2023-01-26 12:06:53 +0100

    feat: flush opcache

--- deployer/typo3cms/task/typo3cms_cache.php
+++ deployer/typo3cms/task/typo3cms_cache.php
@@ -6,7 +6,7 @@ task('typo3cms:cache:flushandwarmup', function () {
     $activeDir = test('[ -e {{deploy_path}}/release ]') ?
         get('deploy_path') . '/release' :
         get('deploy_path') . '/current';
-    run('cd ' . $activeDir . ' &&  {{bin/php}} {{bin/typo3cms}} cache:flush && {{bin/php}} {{bin/typo3cms}} cache:warmup');
+    run('cd ' . $activeDir . ' &&  {{bin/php}} {{bin/typo3cms}} cache:flush && {{bin/php}} {{bin/typo3cms}} opcache:reset && {{bin/php}} {{bin/typo3cms}} cache:warmup');
 });
 
 task('typo3cms:cache:flushfrontend', function () {
