11388b2c43623a689c72dce706922f98fbbc89d8
Tobias Gaertner <tga@xima.de>
2023-01-26 15:47:56 +0100

    fix: split cache flush and warmup tasks

--- deployer/typo3cms/task/typo3cms_cache.php
+++ deployer/typo3cms/task/typo3cms_cache.php
@@ -2,11 +2,31 @@
 
 namespace Deployer;
 
-task('typo3cms:cache:flushandwarmup', function () {
+task('typo3cms:cache:flushandwarmup', [
+    'typo3cms:cache:flush',
+    'typo3cms:cache:warmup'
+]);
+
+task('typo3cms:cache:flush', function () {
     $activeDir = test('[ -e {{deploy_path}}/release ]') ?
         get('deploy_path') . '/release' :
         get('deploy_path') . '/current';
-    run('cd ' . $activeDir . ' &&  {{bin/php}} {{bin/typo3cms}} cache:flush && {{bin/php}} {{bin/typo3cms}} opcache:reset && {{bin/php}} {{bin/typo3cms}} cache:warmup');
+    run('cd ' . $activeDir . ' &&  {{bin/php}} {{bin/typo3cms}} cache:flush');
+});
+
+task('typo3cms:cache:opcachereset', function () {
+    // todo check if opcache is present on server and if opcache command is available
+    $activeDir = test('[ -e {{deploy_path}}/release ]') ?
+        get('deploy_path') . '/release' :
+        get('deploy_path') . '/current';
+    run('cd ' . $activeDir . ' &&  {{bin/php}} {{bin/typo3cms}} opcache:reset');
+});
+
+task('typo3cms:cache:warmup', function () {
+    $activeDir = test('[ -e {{deploy_path}}/release ]') ?
+        get('deploy_path') . '/release' :
+        get('deploy_path') . '/current';
+    run('cd ' . $activeDir . ' && {{bin/php}} {{bin/typo3cms}} cache:warmup');
 });
 
 task('typo3cms:cache:flushfrontend', function () {
