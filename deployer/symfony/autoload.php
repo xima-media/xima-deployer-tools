632e4a335e78ece7bfcd39cd34843f7aa82b1133
Konrad Michalik <konrad.michalik@xima.de>
2023-02-16 15:48:40 +0100

    feat: add further symfony deployer tasks and config

--- deployer/symfony/autoload.php
+++ deployer/symfony/autoload.php
@@ -2,8 +2,13 @@
 
 namespace Deployer;
 
-new \SourceBroker\DeployerExtendedSymfony5\Loader();
+require_once('vendor/sourcebroker/deployer-instance/deployer');
+require_once('vendor/sourcebroker/deployer-extended/deployer');
 
 require_once(__DIR__ . '/config/set.php');
+
+require_once(__DIR__ . '/task/deploy_cache_clear.php.php');
+require_once(__DIR__ . '/task/deploy_cache_warmup.php');
+require_once(__DIR__ . '/task/deploy_database_update.php');
 
 require_once(__DIR__ . '/task/deploy.php');
--- deployer/symfony/config/set.php
+++ deployer/symfony/config/set.php
@@ -5,6 +5,47 @@ namespace Deployer;
 
 set('web_path', 'public/');
 
+set('default_timeout', 900);
+
+set('shared_dirs', [
+        'var/logs',
+        'var/sessions'
+    ]
+);
+
+set('shared_files', [
+        '.env.local.php',
+        '.env.local'
+    ]
+);
+
+set('writable_dirs', [
+    'var'
+]);
+
+set('bin/console', function () {
+    return parse('{{release_path}}/bin/console');
+});
+
+set('console_options', function () {
+    return '--no-interaction';
+});
+
+set('clear_paths', [
+    '.git',
+    '.gitignore',
+    '.gitattributes',
+]);
+
+// Look on https://github.com/sourcebroker/deployer-extended#buffer-start for docs
+set('buffer_config', function () {
+    return [
+        'index.php' => [
+            'entrypoint_filename' => get('web_path') . 'index.php',
+        ],
+    ];
+});
+
 // Look https://github.com/sourcebroker/deployer-extended-media for docs
 set('media',
     [
