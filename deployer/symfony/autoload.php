deb031b4943a8c6fff412c3f6dbc2eefa5aede18
Konrad Michalik <konrad.michalik@xima.de>
2023-02-17 10:27:30 +0100

    feat: WIP fix vendor root path

--- deployer/symfony/autoload.php
+++ deployer/symfony/autoload.php
@@ -2,7 +2,7 @@
 
 namespace Deployer;
 
-$vendorRoot = is_dir(__DIR__ . '/../../../vendor') ? __DIR__ . '/../../..' : __DIR__ . '/../..';
+$vendorRoot = is_dir(__DIR__ . '/../../../../../vendor') ? __DIR__ . '/../../../../..' : __DIR__ . '/../../../..';
 
 require_once($vendorRoot . '/vendor/sourcebroker/deployer-loader/autoload.php');
 new \Xima\XimaDeployerTools\Symfony\Loader();
