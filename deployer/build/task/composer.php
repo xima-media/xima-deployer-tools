e48daa03f7a5b3ebb639f264f3baa4b015e1b614
Konrad Michalik <konrad.michalik@xima.de>
2023-02-20 08:12:42 +0100

    fix: unknown php error using composer

--- deployer/build/task/composer.php
+++ deployer/build/task/composer.php
@@ -3,5 +3,5 @@
 namespace Deployer;
 
 task('build:composer', function () {
-    runLocally('{{bin/composer}} {{composer_action}} -d {{app_path}} {{composer_options}}');
+    runLocally('composer {{composer_action}} -d {{app_path}} {{composer_options}}');
 });
