86df31eb3b0e1edadd8f4cc65b47c8e3e0cf8baa
Konrad Michalik <konrad.michalik@xima.de>
2023-02-20 10:04:47 +0100

    fix: enable database sync for feature branches

--- deployer/feature/task/feature_sync.php
+++ deployer/feature/task/feature_sync.php
@@ -10,12 +10,13 @@ task('feature:sync', function () {
     if ((has('feature_setup') && !get('feature_setup')) || !input()->getOption('feature')) return;
 
     $feature = initFeature();
+    $optionalVerbose = isVerbose() ? '-v' : '';
 
     // ToDo: try https://github.com/sourcebroker/deployer-extended-database
     info('Synching database');
-    //runLocally("{{db_sync_tool}} -f {{feature_sync_config}} --target-path {{feature_sync_target_path}} -y");
+    runLocally("{{db_sync_tool}} -f {{feature_sync_config}} --target-path {{feature_sync_target_path}} -y $optionalVerbose");
     info('Synching files');
-    runLocally("{{file_sync_tool}} -f {{feature_sync_config}} --files-target {{feature_sync_target_path}}");
+    runLocally("{{file_sync_tool}} -f {{feature_sync_config}} --files-target {{feature_sync_target_path}} $optionalVerbose");
     info("feature branch <fg=magenta;options=bold>$feature</> was sucessfully synced");
 
 })
