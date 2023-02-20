8b07e0be3444a31c613dc2d296daf1c29f471300
Konrad Michalik <konrad.michalik@xima.de>
2023-02-20 15:54:52 +0100

    fix: file sync tool config

--- deployer/feature/task/feature_sync.php
+++ deployer/feature/task/feature_sync.php
@@ -16,8 +16,8 @@ task('feature:sync', function () {
     info('Synching database');
     runLocally("{{db_sync_tool}} -f {{feature_sync_config}} --target-path {{feature_sync_target_path}} -y $optionalVerbose");
     info('Synching files');
-    runLocally("{{file_sync_tool}} -f {{feature_sync_config}} --files-target {{feature_sync_target_path}} $optionalVerbose");
-    info("feature branch <fg=magenta;options=bold>$feature</> was sucessfully synced");
+    runLocally("{{file_sync_tool}} -f {{feature_sync_config}} --files-target {{feature_sync_target_path_files}} $optionalVerbose");
+    info("feature branch <fg=magenta;options=bold>$feature</> was successfully synced");
 
 })
     ->select('type=feature-branch-deployment')
