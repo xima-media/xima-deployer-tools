89543939a1de9afcecf89ffbd9be55c294ca10b5
Tobias Gaertner <tga@xima.de>
2023-05-08 12:47:10 +0200

    feat: add default backup for typo3 prod deployment

--- deployer/typo3/task/deploy_database.php
+++ deployer/typo3/task/deploy_database.php
@@ -8,3 +8,10 @@ task('deploy:database:update', function () {
         get('deploy_path') . '/current';
     run('cd ' . $activeDir . ' && {{bin/php}} {{bin/typo3cms}} database:updateschema');
 });
+
+/**
+ * Make Backup of DB in Prod before update
+ */
+task('database:backup')->select('prod');
+before('deploy:database:update', 'database:backup');
+set('sync_database_backup_config', __DIR__ . '/.deployment/db-sync-tool/backup-prod.yaml');
