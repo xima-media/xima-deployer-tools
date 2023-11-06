0727c661c562b38642a3b1ed75af95c3367531e3
Konrad Michalik <konrad.michalik@xima.de>
2023-11-06 09:01:10 +0100

    fix: define own rsync typo3 settings (SCI-385)

--- deployer/rsync/config/set.php
+++ deployer/rsync/config/set.php
@@ -7,13 +7,15 @@ require 'contrib/rsync.php';
 set('repository', '');
 set('default_timeout', 900);
 
+set('rsync-exclude-file', '.deployment/rsync/exclude.txt');
+
 /**
  * rsync
  */
 set('rsync_src', './{{app_path}}');
 set('rsync', [
     'exclude' => [],
-    'exclude-file' => '.deployment/rsync/exclude.txt',
+    'exclude-file' => get('rsync-exclude-file'),
     'include' => [],
     'include-file' => false,
     'filter' => [],
