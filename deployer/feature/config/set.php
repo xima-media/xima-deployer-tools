269860c2372bce0a4454114f37fd05c213914dde
Konrad Michalik <konrad.michalik@xima.de>
2023-12-14 11:00:52 +0100

    fix: change default jira path

--- deployer/feature/config/set.php
+++ deployer/feature/config/set.php
@@ -52,7 +52,7 @@ set('feature_index_app_path', '');
 //    'title' => 'path'
 //]);
 //set('feature_index_app_type', 'symfony');
-set('feature_index_jira_browse', 'https://jira.xima.de/browse/');
+set('feature_index_jira_browse', 'https://xima.atlassian.net/browse/');
 set('feature_index_jira_api', 'https://jira.xima.de/rest/api/2/issue/');
 set('feature_index_jira_auth', '');
 set('feature_index_git_branch', '');
