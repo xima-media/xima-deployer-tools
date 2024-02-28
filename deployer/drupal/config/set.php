d7c38ddd7dd9f3dae49c80c0821cd07f95973ded
Dennis Patzer <dpa@xima.de>
2024-02-28 13:25:32 +0100

    feat: add real time output for drupal and typo3

--- deployer/drupal/config/set.php
+++ deployer/drupal/config/set.php
@@ -28,6 +28,8 @@ set('writable_chmod_recursive', false);
 
 set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --no-scripts');
 
+set('run_real_time_output', true);
+
 // Look on https://github.com/sourcebroker/deployer-extended#buffer-start for docs
 set('buffer_config', function () {
     return [
