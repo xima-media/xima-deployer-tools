3b8081307da08277855d16d25179b4e4b6a93e17
Konrad Michalik <konrad.michalik@xima.de>
2024-04-09 11:11:09 +0200

    fix: composer dev branch issue

--- deployer/dev/task/start_new_release.php
+++ deployer/dev/task/start_new_release.php
@@ -7,15 +7,15 @@ task('dev:release:start_new_release', function () {
     info("[step] start new release");
     $version = get('newVersion');
 
-    /* new git branch */
-    debug("create new branch: \"release-$version\"");
-    runLocally("git checkout -b \"release-$version\"");
-
     /* new release version */
     debug("set new version: \"{$version}-RC\"");
     runLocally("composer config version {$version}-RC --working-dir " . get('composer_path_app'));
     runLocally("composer update nothing -q " . get('composer_app_release_options') . " --working-dir " . get('composer_path_app'));
     runLocally("composer validate --working-dir " . get('composer_path_app'));
+
+    /* new git branch */
+    debug("create new branch: \"release-$version\"");
+    runLocally("git checkout -b \"release-$version\"");
 
     debug("git commit");
     runLocally("git add " . get('composer_path_app') . "/composer.json " . get('composer_path_app') . "/composer.lock");
