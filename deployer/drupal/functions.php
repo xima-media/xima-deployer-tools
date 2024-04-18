f82ef8425265d90c16c034407d348769928df3c0
Dennis Patzer <dpa@xima.de>
2024-04-18 16:01:53 +0200

    feat: add drupal specific logging data

--- deployer/drupal/functions.php
+++ deployer/drupal/functions.php
@@ -5,7 +5,7 @@ namespace Deployer;
 function getDebugLogApp(array $logLines): array
 {
     $log = [
-        get('debug_log_default_header')
+        array_merge(get('debug_log_default_header'), ['Context', 'Extra'])
     ];
     foreach ($logLines as $key => $logLine) {
         if (preg_match(get('debug_log_regex_pattern'), $logLine, $matches)) {
@@ -13,7 +13,9 @@ function getDebugLogApp(array $logLines): array
                 $key,
                 $matches[1],
                 $matches[3],
-                $matches[4]
+                $matches[4],
+                $matches[6],
+                $matches[8]
             ];
         }
     }
