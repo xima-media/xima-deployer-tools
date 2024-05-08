9ff86c6b6d5984d10928df8ff8c23f8b5f56ac18
Dennis Patzer <dpa@xima.de>
2024-05-08 11:01:17 +0200

    fix: restore tabula_rasa sub tasks in drupal context

--- deployer/drupal/task/tabula_rasa.php
+++ deployer/drupal/task/tabula_rasa.php
@@ -13,7 +13,7 @@ task('dev:tabula_rasa:post_db_sync', function() {
 task('dev:tr', [
   'dev:tabula_rasa:composer_install_app',
   'dev:tabula_rasa:composer_install_ci',
-//  'dev:tabula_rasa:npm_build',
-//  'dev:tabula_rasa:db_sync',
+  'dev:tabula_rasa:npm_build',
+  'dev:tabula_rasa:db_sync',
   'dev:tabula_rasa:post_db_sync',
 ]);
