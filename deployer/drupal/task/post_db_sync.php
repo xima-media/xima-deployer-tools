4b532a7870f94aaedd5672886f460e2cdb54e092
Dennis Patzer <dpa@xima.de>
2024-05-08 13:30:49 +0200

    refactor: task arrangement for drupal

--- deployer/drupal/task/post_db_sync.php
+++ deployer/drupal/task/post_db_sync.php
@@ -2,11 +2,11 @@
 
 namespace Deployer;
 
-task('dev:tabula_rasa:post_db_sync', function() {
+task('dev:post_db_sync', function() {
   runExtended('drush cache-rebuild');
   runExtended('drush updb -y');
   runExtended('drush cim -y');
   runExtended('drush cache-rebuild');
 });
 
-after('dev:sync', 'dev:tabula_rasa:post_db_sync');
+after('dev:sync', 'dev:post_db_sync');
