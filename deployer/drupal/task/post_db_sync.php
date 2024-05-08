18834f7c1100f371b744f3839fef643634b38e62
Dennis Patzer <dpa@xima.de>
2024-05-08 13:29:09 +0200

    refactor: task arrangement for drupal

--- deployer/drupal/task/post_db_sync.php
+++ deployer/drupal/task/post_db_sync.php
@@ -9,11 +9,4 @@ task('dev:tabula_rasa:post_db_sync', function() {
   runExtended('drush cache-rebuild');
 });
 
-// override dev command
-task('dev:tr', [
-  'dev:tabula_rasa:composer_install_app',
-  'dev:tabula_rasa:composer_install_ci',
-  'dev:tabula_rasa:npm_build',
-  'dev:tabula_rasa:db_sync',
-  'dev:tabula_rasa:post_db_sync',
-]);
+after('dev:sync', 'dev:tabula_rasa:post_db_sync');
