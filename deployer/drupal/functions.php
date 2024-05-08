d01fb5852c3957365626104d114d53496a558c3b
Dennis Patzer <dpa@xima.de>
2024-05-08 11:47:19 +0200

    fix: drupal logging

--- deployer/drupal/functions.php
+++ deployer/drupal/functions.php
@@ -4,18 +4,14 @@ namespace Deployer;
 
 function getDebugLogApp(array $logLines): array
 {
-    $log = [
-        array_merge(get('debug_log_default_header'), ['Context', 'Extra'])
-    ];
+    $log = [get('debug_log_default_header')];
     foreach ($logLines as $key => $logLine) {
         if (preg_match(get('debug_log_regex_pattern'), $logLine, $matches)) {
             $log[] = [
                 $key,
                 $matches[1],
                 $matches[3],
-                $matches[4],
-                $matches[6],
-                $matches[8]
+                $matches[4]
             ];
         }
     }
