958eef3c0bd1c579a74bc51798db362a35a4b5fc
Fabian Hänchen <fabian.haenchen@xima.de>
2025-04-08 13:26:45 +0200

    add webhook default version

--- deployer/notification/config/set.php
+++ deployer/notification/config/set.php
@@ -6,5 +6,7 @@ set('msteams_webhook', null);
 set('msteams_text', 'Der Branch **[{{feature}}]({{public_url}})** wurde bereitgestellt.');
 set('msteams_color', '#2E7D32');
 
+set('msteams_webhook_version', 1);
+
 set('deploy_prod_notify_text', 'Auf **[{{deploy_target}}]({{public_url}})** wurde die Version **{{deploy_version}}** veröffentlicht.');
 set('deploy_prod_notify_color', '#B71C1C');
