036a61840e014c35f3077c14ff3567e852e6b119
Fabian Hänchen <fabian.haenchen@xima.de>
2025-04-08 10:33:23 +0200

    Add support for workflow style ms teams webhooks

--- deployer/notification/task/ms_teams.php
+++ deployer/notification/task/ms_teams.php
@@ -12,12 +12,11 @@ task('msteams:notify', function () {
 
 
 /**
- * @param $message
- * @param $color
+ * @param string $message Message to be displayed in MS Teams.
+ * @param string $color Highlight color. Not in use with webhook v2.
  * @return void
- * @throws \Deployer\Exception\GracefulShutdownException
  */
-function sendMessage($message, $color): void
+function sendMessage(string $message = '', string $color = ''): void
 {
     if (!empty(getenv('DEPLOYER_CONFIG_NOTIFICATION_MUTE'))) {
         debug("skipping notification because of DEPLOYER_CONFIG_NOTIFICATION_MUTE environment variable");
@@ -29,14 +28,43 @@ function sendMessage($message, $color): void
         throw new GracefulShutdownException('Missing MS Teams webhook for notification task. Use "msteams_webhook" deployer configuration or "DEPLOYER_CONFIG_NOTIFICATION_MSTEAMS_WEBHOOK" environment variable to define the necessary webhook url.');
     }
 
+    if (has('msteams_webhook_version') && get('msteams_webhook_version') == 2) {
+      // Make it possible to define entirely customized messages and only use the simple card as a fallback.
+      if (has('msteams_request_body')) {
+        $body = get('msteams_request_body');
+      } else {
+        $body = [
+          "type" => "message",
+          "attachments" => [
+            [
+              "contentType" => "application/vnd.microsoft.card.adaptive",
+              "content" => [
+                "\$schema" => "http://adaptivecards.io/schemas/adaptive-card.json",
+                "type" => "AdaptiveCard",
+                "version" => "1.0",
+                "body" => [
+                  [
+                    "type" => "TextBlock",
+                    "text" => $message,
+                    "weight" => "bolder",
+                    "size" => "medium",
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ];
+      }
+    } else {
+      $body = [
+        "themeColor" => $color,
+        'text'       => $message
+      ];
+    }
+
     $curlTimeout = getenv('DEPLOYER_CONFIG_NOTIFICATION_MSTEAMS_TIMEOUT') ?: 10;
     Httpie::post(get('msteams_webhook'))
         ->setopt(CURLOPT_TIMEOUT, $curlTimeout)
-        ->jsonBody(
-            [
-                "themeColor" => $color,
-                'text'       => $message
-            ]
-        )
+        ->jsonBody($body)
         ->send();
 }
