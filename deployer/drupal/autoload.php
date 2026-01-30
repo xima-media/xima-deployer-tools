7d03d0c9f0f363a75bed8e3e4750ea4d6182eb8d
Fabian Hänchen <fabian.haenchen@xima.de>
2026-01-30 11:57:49 +0100

    fix: update autoload

--- deployer/drupal/autoload.php
+++ deployer/drupal/autoload.php
@@ -17,15 +17,11 @@ require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/fun
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/config/options.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/config/set.php');
 
-require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_cache.php');
-require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_configuration.php');
+require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_drush.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_database.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_files.php');
-require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_hook.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_logs.php');
-require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_maintenance.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_permissions.php');
-require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/deploy_translations.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/dump.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/import.php');
 require_once($vendorRoot . '/vendor/xima/xima-deployer-tools/deployer/drupal/task/post_db_sync.php');
